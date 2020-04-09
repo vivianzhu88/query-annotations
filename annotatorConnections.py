24fdc38ce4e05bb0812b3b763f4877f1ea0f9a22
vivianzhu88 <31047942+vivianzhu88@users.noreply.github.com>
2020-04-09 10:49:47 -0700

    attempt POST URI

--- annotatorConnections.py
+++ annotatorConnections.py
@@ -1,4 +1,5 @@
 import urllib.request, urllib.error, urllib.parse
+import requests
 import json
 import os
 import openpyxl
@@ -7,17 +8,20 @@ from pprint import pprint
 REST_URL = "http://data.bioontology.org"
 ONT = "&ontologies=RADLEX"
 API_KEY = ""
-
-def get_json(url):
-#get annotations
-    opener = urllib.request.build_opener()
-    opener.addheaders = [('Authorization', 'apikey token=' + API_KEY)]
-    return json.loads(opener.open(url).read())
-
-def mapRIDs(annotations, get_class=True):
-#take annotations and map to Excel spreadsheet of labels and corresponding RIDs
-    annDict = {}
     
+def get_annotations(text, url):
+    headers = {'Authorization': 'apikey token=' + API_KEY}
+    data = text
+
+    response = requests.request("POST",url,headers=headers,data=data)
+    print(response.text)
+    return json.loads(response.text)
+    
+def mapRIDs(text, get_class=True):
+#take annotations and map to Excel spreadsheet of labels and corresponding RIDs
+    annotations = get_annotations(text, REST_URL + "/annotator?text=" + urllib.parse.quote(text) + ONT)
+    
+    RIDs = []
     #iterate through annotations
     for result in annotations:
         class_details = result["annotatedClass"]
@@ -30,16 +34,30 @@ def mapRIDs(annotations, get_class=True):
         
         #get each RIDs correlating with text
         id = class_details["@id"]
-        RID = id[22:]
-        for annotation in result["annotations"]:
-            label = annotation["text"]
-        
-        #map all RIDs corresponding to a text into a label
-        if label in annDict:
-            annDict[label].append(RID)
-        else:
-            annDict[label] = [RID]
+        RIDs.append(id[22:])
+    return RIDs
 
+class File():
+    def __init__(self, filename):
+    #initialize each file
+        self.filename = filename
+        self.RIDs = []
+        self.text = ""
+    
+    def openFile(self):
+    #open file and retrieve text
+    #has .ore .txt .lsx .ocx .csv .xls .son .xml files
+        with open(self.filename, 'rb') as file:
+            self.text = file.read()
+    
+    def getName(self):
+        return self.filename
+    
+    def getRIDs(self):
+        self.RIDs = mapRIDs(self.text)
+        return self.RIDs
+
+def toSpreadsheet(RIDs):
     #put data into to Excel spreadsheet
     workbook = openpyxl.Workbook()
     sheet = workbook.active
@@ -49,13 +67,23 @@ def mapRIDs(annotations, get_class=True):
         sheet.cell(row=row, column=2, value=', '.join(values))
         row += 1
 
-    workbook.save(filename="labels+RIDs.xlsx")
+    workbook.save(filename="fileRIDs.xlsx")
 
-#input for Annotator
-text_to_annotate = ("There is a mass on the right lower lobe of the lung.")
+#put all of file paths in Chest_and_Lung_Collections directory into a list
+filesList = []
+RIDsDict = {}
 
-#get annotations
-annotations = get_json(REST_URL + "/annotator?text=" + urllib.parse.quote(text_to_annotate) + ONT)
-mapRIDs(annotations)
+dir_path = "/Users/vivianzhu/Documents/Annotator/Chest_and_Lung_Collections/"
+for dp,_,filenames in os.walk(dir_path):
+   for f in filenames:
+       f = File(os.path.abspath(os.path.join(dp, f))[len(dir_path):])
+       filesList.append(f)
 
+for f in filesList:
+    f.openFile()
+    RIDsDict[f.getName()] = f.getRIDs()
+
+toSpreadsheet(RIDsDict)
+    
+    
 

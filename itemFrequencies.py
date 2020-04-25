5439f261944a99888ee09d04fb809e3c3538c110
vivianzhu88 <31047942+vivianzhu88@users.noreply.github.com>
2020-04-25 00:02:39 -0700

    combined freq_RIDs.xlsx and freq_Rterms.xlsx

--- itemFrequencies.py
+++ itemFrequencies.py
@@ -10,12 +10,23 @@ class RawData():
         self.sheet = self.workbook.active
         self.rDict = {}
         self.sortedKeys = []
+    
+    def getItemName(self):
+    #return item name
+        return self.itemName
+    
+    def getItems(self):
+    #return list of RadLex RIDs or Rterms
+        items = []
+        for k in self.sortedKeys:
+            items.append(k)
+        return items
 
     def dictOfterms(self):
     #create dictionary where key is the RadLex item, values are the filepaths
         for row in self.sheet.iter_rows(min_row=2, values_only=True):
             filepath, radlex = row
-            rs = radlex.split(", ")
+            rs = radlex.split(" | ")
             for r in rs:
                 if r in self.rDict:
                     self.rDict[r].append(filepath)
@@ -26,32 +37,35 @@ class RawData():
     #sort by number
         self.sortedKeys = sorted(self.rDict, key=lambda k: len(self.rDict[k]), reverse=True)
     
-    def writeResults(self):
+    def writeResults(self, rawdata):
     #frequencies of all RadLex items sorted in order of most to least frequent
         workbook = openpyxl.Workbook()
         sheet = workbook.active
         sheet.cell(row=1, column=1, value=self.itemName)
-        sheet.cell(row=1, column=2, value="Count")
-        sheet.cell(row=1, column=3, value="File Paths")
+        sheet.cell(row=1, column=2, value=rawdata.getItemName())
+        sheet.cell(row=1, column=3, value="Count")
+        sheet.cell(row=1, column=4, value="File Paths")
         
+        rdItems = rawdata.getItems()
         row = 2
         for k in self.sortedKeys:
             sheet.cell(row=row, column=1, value=k)
-            sheet.cell(row=row, column=2, value=len(self.rDict[k]))
-            sheet.cell(row=row, column=3, value=', '.join(self.rDict[k]))
+            sheet.cell(row=row, column=2, value=rdItems[row-2])
+            sheet.cell(row=row, column=3, value=len(self.rDict[k]))
+            sheet.cell(row=row, column=4, value=' | '.join(self.rDict[k]))
             row += 1
     
-        fN = "freq_" + self.itemName + ".xlsx"
+        fN = "freq.xlsx"
         workbook.save(filename=fN)
     
     def analyze(self):
     #call all the necessary methods in order
         self.dictOfterms()
         self.sortByLen()
-        self.writeResults()
     
 
 IDs = RawData("filepath_and_RIDs.xlsx", "RIDs")
 IDs.analyze()
 Rterms = RawData("filepath_and_Rterms.xlsx", "Rterms")
 Rterms.analyze()
+IDs.writeResults(Rterms)

dbe00279ae74731f57e7fe783650b02849ec3006
vivianzhu88 <31047942+vivianzhu88@users.noreply.github.com>
2020-06-26 15:34:02 -0700

    add more UID values

--- linkImg.py
+++ linkImg.py
@@ -24,8 +24,12 @@ def createSpreadsheet():
     sheet.cell(row=1, column=12, value="Texture")
     sheet.cell(row=1, column=13, value="Malignancy")
     sheet.cell(row=1, column=14, value="Image Z Postion")
-    sheet.cell(row=1, column=15, value="imageSop_UID")
-    sheet.cell(row=1, column=16, value="XY Coordinates")
+    sheet.cell(row=1, column=15, value="LIDC ReadMessage UID")
+    sheet.cell(row=1, column=16, value="IDRI ReadMessage UID")
+    sheet.cell(row=1, column=17, value="SeriesInstanceUID")
+    sheet.cell(row=1, column=18, value="StudyInstanceUID")
+    sheet.cell(row=1, column=19, value="imageSop_UID")
+    sheet.cell(row=1, column=20, value="XY Coordinates")
     workbook.save(filename="LIDC-IDRI.xlsx")
     
 def toSpreadsheet(data, rterm, rid):
@@ -50,6 +54,10 @@ def toSpreadsheet(data, rterm, rid):
         sheet.cell(row=r, column=14, value=d[13])
         sheet.cell(row=r, column=15, value=d[14])
         sheet.cell(row=r, column=16, value=d[15])
+        sheet.cell(row=r, column=17, value=d[16])
+        sheet.cell(row=r, column=18, value=d[17])
+        sheet.cell(row=r, column=19, value=d[18])
+        sheet.cell(row=r, column=20, value=d[19])
     
     workbook.save(filename="LIDC-IDRI.xlsx")
     
@@ -71,15 +79,19 @@ def toJson(data):
         x["Texture"] = d[11]
         x["Malignancy"] = d[12]
         x["Image Z Postion"] = d[13]
-        x["imageSop_UID"] = d[14]
-        x["XY Coordinates"] = d[15]
+        x["LIDC ReadMessage UID"] = d[14]
+        x["IDRI ReadMessage UID"] = d[15]
+        x["SeriesInstanceUID"] = d[16]
+        x["StudyInstanceUID"] = d[17]
+        x["imageSop_UID"] = d[18]
+        x["XY Coordinates"] = d[19]
         output.append(x)
         
     with open("LIDC-IDRI.json", "w") as outfile:
         json.dump(output, outfile)
 
 def toCSV(data):
-    fields = ["RadLex Term", "RadLex ID", "Nodule/NonNodule", "Nodule/NonNodule ID", "Subtlety", "Internal Structure", "Calcification", "Sphericity", "Margin", "Lobulation", "Spiculation", "Texture", "Malignancy", "Image Z Postion", "imageSop_UID", "XY Coordinates"]
+    fields = ["RadLex Term", "RadLex ID", "Nodule/NonNodule", "Nodule/NonNodule ID", "Subtlety", "Internal Structure", "Calcification", "Sphericity", "Margin", "Lobulation", "Spiculation", "Texture", "Malignancy", "Image Z Postion", "LIDC ReadMessage UID", "IDRI ReadMessage UID", "SeriesInstanceUID", "StudyInstanceUID", "imageSop_UID", "XY Coordinates"]
     rows = data
     filename = "LIDC-IDRI.csv"
     
@@ -88,22 +100,33 @@ def toCSV(data):
         csvwriter.writerow(fields)
         csvwriter.writerows(rows)
 
-def checkTag(tag):
+def checkTag(tag, v=None):
     if tag:
-        return tag[0].firstChild.nodeValue
+        if v is None:
+            return tag[0].firstChild.nodeValue
+        else:
+            return tag[0].attributes.get("uid").value
     else:
         return ""
+
 
 def getData(f):
     data = []
     try:
         filepath = "Chest_and_Lung_Collections/" + f
         xml_data = minidom.parse("Chest_and_Lung_Collections/" + f)
+        print(f)
         
     except (FileNotFoundError, OSError):
         print(filepath)
         return
     
+    lidc_uid = checkTag(xml_data.getElementsByTagName('LidcReadMessage'),"v")
+    idri_uid = checkTag(xml_data.getElementsByTagName('IdriReadMessage'),"v")
+    series_uid = checkTag(xml_data.getElementsByTagName('SeriesInstanceUid'))
+    if series_uid == "":
+        checkTag(xml_data.getElementsByTagName('SeriesInstanceUID'))
+    study_uid = xml_data.getElementsByTagName('StudyInstanceUID')[0].firstChild.nodeValue
     nodes = xml_data.getElementsByTagName('unblindedReadNodule')
     for n in nodes:
         ID = n.getElementsByTagName('noduleID')[0].firstChild.nodeValue
@@ -120,7 +143,7 @@ def getData(f):
         
         for r in rois:
             z_pos = r.getElementsByTagName('imageZposition')[0].firstChild.nodeValue
-            UID = n.getElementsByTagName('imageSOP_UID')[0].firstChild.nodeValue
+            sop_UID = n.getElementsByTagName('imageSOP_UID')[0].firstChild.nodeValue
             x_coord = r.getElementsByTagName('xCoord')
             y_coord = r.getElementsByTagName('yCoord')
             
@@ -130,7 +153,7 @@ def getData(f):
                 xy_coords.append(str(xy))
             xys = " | ".join(xy_coords)
                 
-            d = ["Nodule", ID, subtlety, internalStructure, calcification,  sphericity, margin, lobulation, spiculation, texture, malignancy, z_pos, UID, xys]
+            d = ["Nodule", ID, subtlety, internalStructure, calcification,  sphericity, margin, lobulation, spiculation, texture, malignancy, z_pos, lidc_uid, idri_uid, series_uid, study_uid, sop_UID, xys]
             data.append(d)
         
     nodes = xml_data.getElementsByTagName('nonNodule')
@@ -146,8 +169,8 @@ def getData(f):
             xy_coords.append(str(xy))
         xys = " | ".join(xy_coords)
             
-        UID = n.getElementsByTagName('imageSOP_UID')[0].firstChild.nodeValue
-        d = ["NonNodule", ID, "", "", "", "", "", "", "", "", "", z_pos, UID, xys]
+        sop_UID = n.getElementsByTagName('imageSOP_UID')[0].firstChild.nodeValue
+        d = ["NonNodule", ID, "", "", "", "", "", "", "", "", "", z_pos, lidc_uid, idri_uid, series_uid, study_uid, sop_UID, xys]
         data.append(d)
     
     global filedata

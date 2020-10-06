12f00d64159b70dfcdac6b8bc30483ebae8603d8
vivianzhu88 <31047942+vivianzhu88@users.noreply.github.com>
2020-10-06 01:25:56 -0700

    restructure

--- aim converter/Backend.js
+++ aim converter/Backend.js
@@ -26,196 +26,156 @@ function getTemplateAnswers(metadata, annotationName, tempModality) {
       const name = { value: annotationName };
       const typeCode = [
         {
-          code: 'ROI',
-          codeSystemName: '99EPAD',
-          'iso:displayName': { 'xmlns:iso': 'uri:iso.org:21090', value: 'ROI Only' },
+          code: 'LIDC-IDRI',
+          codeSystemName: 'LIDC-IDRI',
+          'iso:displayName': { 'xmlns:iso': 'uri:iso.org:21090', value: 'LIDC-IDRI' },
         },
       ];
       return { comment, modality, name, typeCode };
     }
-  }
-
-export function jsonToAim(jsonObj){
-    this.seedData = {};
-    this.seedData.aim = {};
-    this.seedData.study = {};
-    this.seedData.series = {};
-    this.seedData.equipment = {};
-    this.seedData.person = {};
-    this.seedData.image = [];
-
-    this.seedData.aim.studyInstanceUid = jsonObj['StudyInstanceUID'];
-    this.seedData.study.startTime = "";
-    this.seedData.study.instanceUid = jsonObj['StudyInstanceUID'];
-    this.seedData.study.startDate = "";
-    this.seedData.study.accessionNumber = "";
-    this.seedData.series.instanceUid = jsonObj['SeriesInstanceUID'];
-    this.seedData.series.modality = jsonObj['Modality'];
-    this.seedData.series.number = "";
-    this.seedData.series.description = "";
-    this.seedData.series.instanceNumber = "";
-    this.seedData.equipment.manufacturerName = "";
-    this.seedData.equipment.manufacturerModelName = "";
-    this.seedData.equipment.softwareVersion = "";
-    this.seedData.person.sex = "";
-    this.seedData.person.name = "";
-    this.seedData.person.patientId = jsonObj['Patient ID'];
-    this.seedData.person.birthDate = "";
-    this.sopClassUid = jsonObj['SOPClassUID'];
-    this.sopInstanceUid = jsonObj['SOPInstanceUID'];
 }
 
-// fill in the seed data
+function getCharacteristicsData(){
+    data = {
+        "typeCode": [
+            {
+                "code": code,
+                "codeSystemName": systemName,
+                "iso:displayName": {
+                    "value": value,
+                    "xmlns:iso": xmlns
+                }
+            }
+        ],
+        "annotatorConfidence": {
+            "value": 0
+        },
+        "label": {
+            "value": "margin"
+        }
+    }
+    return {data};
+}
 
-seedData.aim.imagingObservationEntityCollection = {
-    "ImagingObservationEntity": [
-        {
-            "typeCode": [
+export function jsonToAim(jsonObj){
+    const seedData = {};
+    seedData.aim = {};
+    seedData.study = {};
+    seedData.series = {};
+    seedData.equipment = {};
+    seedData.person = {};
+    seedData.image = [];
+
+    seedData.aim.studyInstanceUid = jsonObj['StudyInstanceUID'];
+    seedData.study.startTime = "";
+    seedData.study.instanceUid = jsonObj['StudyInstanceUID'];
+    seedData.study.startDate = "";
+    seedData.study.accessionNumber = "";
+    seedData.series.instanceUid = jsonObj['SeriesInstanceUID'];
+    seedData.series.modality = jsonObj['Modality'];
+    seedData.series.number = "";
+    seedData.series.description = "";
+    seedData.series.instanceNumber = "";
+    seedData.equipment.manufacturerName = "";
+    seedData.equipment.manufacturerModelName = "";
+    seedData.equipment.softwareVersion = "";
+    seedData.person.sex = "";
+    seedData.person.name = "";
+    seedData.person.patientId = jsonObj['Patient ID'];
+    seedData.person.birthDate = "";
+    
+    const sopClassUid = jsonObj['SOPClassUID'];
+    const sopInstanceUid = jsonObj['SOPInstanceUID'];
+
+    if (jsonObj['Nodule/NonNodule'] == 'Nodule'){
+
+        var theChars = []
+        if (jsonObj['Subtlety'] != ''){
+            theChars.push(getCharacteristicsData('Subtlety', jsonObj['Subtlety']))
+        }
+        if (jsonObj['Internal Structure'] != ''){
+            theChars.push(getCharacteristicsData('Internal Structure', jsonObj['Internal Structure']))
+        }
+        if (jsonObj['Calcification'] != ''){
+            theChars.push(getCharacteristicsData('Calcification', jsonObj['Calcification']))
+        }
+        if (jsonObj['Sphericity'] != ''){
+            theChars.push(getCharacteristicsData('Sphericity', jsonObj['Sphericity']))
+        }
+        if (jsonObj['Margin'] != ''){
+            theChars.push(getCharacteristicsData('Margin', jsonObj['Margin']))
+        }
+        if (jsonObj['Lobulation'] != ''){
+            theChars.push(getCharacteristicsData('Lobulation', jsonObj['Lobulation']))
+        }
+        if (jsonObj['Spiculation'] != ''){
+            theChars.push(getCharacteristicsData('Spiculation', jsonObj['Spiculation']))
+        }
+        if (jsonObj['Texture'] != ''){
+            theChars.push(getCharacteristicsData('Texture', jsonObj['Texture']))
+        }
+        if (jsonObj['Malignancy'] != ''){
+            theChars.push(getCharacteristicsData('Malignancy', jsonObj['Malignancy']))
+        }
+
+        seedData.aim.imagingObservationEntityCollection = {
+            "ImagingObservationEntity": [
                 {
-                    "code": "nodule",
-                    "codeSystemName": "LIDC-IDRI",
-                    "iso:displayName": {
-                        "value": "nodule",
-                        "xmlns:iso": "uri:iso.org:21090"
+                    "typeCode": [
+                        {
+                            "code": "nodule",
+                            "codeSystemName": "LIDC-IDRI",
+                            "iso:displayName": {
+                                "value": "nodule",
+                                "xmlns:iso": "uri:iso.org:21090"
+                            }
+                        }
+                    ],
+                    "annotatorConfidence": {
+                        "value": 0
+                    },
+                    "label": {
+                        "value": "Nodule"
+                    },
+                    "uniqueIdentifier": {
+                        "root": generateUid()
+                    },
+                    "imagingObservationCharacteristicCollection": {
+                        "ImagingObservationCharacteristic": theChars
                     }
                 }
-            ],
-            "annotatorConfidence": {
-                "value": 0
-            },
-            "label": {
-                "value": "Nodule"
-            },
-            "uniqueIdentifier": {
-                "root": "2.25.870780621633412196796041908505416068723"
-            },
-            "imagingObservationCharacteristicCollection": {
-                "ImagingObservationCharacteristic": [
-                    {
-                        "typeCode": [
-                            {
-                                "code": "very subtle",
-                                "codeSystemName": "LIDC-IDRI",
-                                "iso:displayName": {
-                                    "value": "very subtle",
-                                    "xmlns:iso": "uri:iso.org:21090"
-                                }
-                            }
-                        ],
-                        "annotatorConfidence": {
-                            "value": 0
-                        },
-                        "label": {
-                            "value": "subtlety"
-                        }
-                    },
-                    {
-                        "typeCode": [
-                            {
-                                "code": "C12471",
-                                "codeSystemName": "NCIT",
-                                "codeSystemVersion": "1.0",
-                                "iso:displayName": {
-                                    "value": "soft tissue",
-                                    "xmlns:iso": "uri:iso.org:21090"
-                                }
-                            }
-                        ],
-                        "annotatorConfidence": {
-                            "value": 0
-                        },
-                        "label": {
-                            "value": "internal structure"
-                        }
-                    },
-                    {
-                        "typeCode": [
-                            {
-                                "code": "popcorn",
-                                "codeSystemName": "LIDC-IDRI",
-                                "iso:displayName": {
-                                    "value": "popcorn",
-                                    "xmlns:iso": "uri:iso.org:21090"
-                                }
-                            }
-                        ],
-                        "annotatorConfidence": {
-                            "value": 0
-                        },
-                        "label": {
-                            "value": "calcification"
-                        }
-                    },
-                    {
-                        "typeCode": [
-                            {
-                                "code": "RID5800",
-                                "codeSystemName": "RadLex",
-                                "codeSystemVersion": "1.0",
-                                "iso:displayName": {
-                                    "value": "oval",
-                                    "xmlns:iso": "uri:iso.org:21090"
-                                }
-                            }
-                        ],
-                        "annotatorConfidence": {
-                            "value": 0
-                        },
-                        "label": {
-                            "value": "sphericity"
-                        }
-                    },
-                    {
-                        "typeCode": [
-                            {
-                                "code": "fairly defined",
-                                "codeSystemName": "LIDC-IDRI",
-                                "iso:displayName": {
-                                    "value": "fairly defined",
-                                    "xmlns:iso": "uri:iso.org:21090"
-                                }
-                            }
-                        ],
-                        "annotatorConfidence": {
-                            "value": 0
-                        },
-                        "label": {
-                            "value": "margin"
-                        }
-                    },
-                    
-                ]
-            }
-        }
-    ]
-};
-seedData.image.push({ sopClassUid, sopInstanceUid });
+            ]
+        };
+        //seedData.image.push({ sopClassUid, sopInstanceUid });
+    }
+    
+    const answers = getTemplateAnswers(seedData, jsonObj['Nodule/NonNodule ID'], '');
+    const merged = { ...seedData.aim, ...answers };
+    seedData.aim = merged;
+    seedData.user = { loginName: 'admin', name: 'admin' }
+    
+    const aim = new Aim(seedData, enumAimType.imageAnnotation);
+    /*
+    // add the markups
+    // points is an array of items { x: parseFloat(x), y: parseFloat(y) }
+    aim.addMarkupEntity(
+       "TwoDimensionPolyline",
+       1,
+       points, // points of the roi in first image
+       imageReferenceUid, // first image
+       1
+     );
+    aim.addMarkupEntity(
+         "TwoDimensionPolyline",
+         2,
+         points, // points of the roi in 2nd image
+         imageReferenceUid, // 2nd image imge sop instance uid
+         1
+       );
+    // add characteristics
+    */
+    console.log(JSON.stringify(aim.getAimJSON())); // to get the aim json seedDataect
+    
+}
 
-
-const answers = getTemplateAnswers(seedData, 'nodule1', '');
-const merged = { ...seedData.aim, ...answers };
-seedData.aim = merged;
-seedData.user = { loginName: 'admin', name: 'admin' }
-
-const aim = new Aim(seedData, enumAimType.imageAnnotation);
-
-// add the markups
-// points is an array of items { x: parseFloat(x), y: parseFloat(y) }
-// aim.addMarkupEntity(
-//   "TwoDimensionPolyline",
-//   1,
-//   points, // points of the roi in first image
-//   imageReferenceUid, // first image
-//   1
-// );
-// aim.addMarkupEntity(
-//     "TwoDimensionPolyline",
-//     2,
-//     points, // points of the roi in 2nd image
-//     imageReferenceUid, // 2nd image imge sop instance uid
-//     1
-//   );
-// add characteristics
-
-console.log(JSON.stringify(aim.getAimJSON())); // to get the aim json seedDataect
 

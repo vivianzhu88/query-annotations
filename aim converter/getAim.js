e4ff42af4983f540ff04ddbe80032c0846d2b577
vivianzhu88 <31047942+vivianzhu88@users.noreply.github.com>
2021-02-23 21:58:07 -0800

    aim per nodule

--- aim converter/getAim.js
+++ aim converter/getAim.js
@@ -1,10 +1,12 @@
 const fs = require('fs');
 const papa = require('papaparse');
 const file = fs.createReadStream("new_LIDC-IDRI.csv");
-const backend = require('Backend')
+const jsonToAim = require('./Backend.js')
+const localStorage = require("localStorage")
 
 //CSV to JSON, get entries in table
-var csvData=[];
+const csvData=[];
+localStorage.clear();
 papa.parse(file, {
   header: true,
   step: function(result) {
@@ -12,15 +14,52 @@ papa.parse(file, {
   },
   complete: function(results, file) {
     //console.log('Complete', csvData.length, 'records.'); 
-    console.log(csvData);
+    //console.log(csvData);
+    //parse the array
+
+    /*
+    for (var i = 0; i < csvData.length; i++){
+      var jsonObj = csvData[i];
+      console.log(jsonObj);
+      var aimObj = jsonToAim(jsonObj);
+      console.log(aimObj);
+    }
+    */
+    console.log('begin data parsing');
+    var modData = []
+    for (var i = 0; i < csvData.length; i++){
+
+      var entry = csvData[i]
+      var patient = entry['PatientID'];
+      var nod = entry['Nodule/NonNodule ID'];
+
+      node = modData.find(node => node['PatientID'] === patient && node['Nodule/NonNodule ID'] === nod);
+
+      if (node){ // nodule + patient id combo exists
+        index = modData.indexOf(node);
+
+        modData[index]['imageSop_UID'].push(entry['imageSop_UID'])
+        modData[index]['XY Coordinates'].push(entry['XY Coordinates'])
+      }
+      else {
+        entry['imageSop_UID'] = [entry['imageSop_UID']]
+        entry['XY Coordinates'] = [entry['XY Coordinates']]
+        modData.push(entry)
+      }
+
+      console.log(modData.length);
+    }
+
+    console.log('begin creating JSON');
+    for (var j = 0; j < modData.length; j++){
+      console.log(j);
+      var jsonObj = modData[j];
+      console.log(jsonObj);
+      var aimObj = jsonToAim(jsonObj);
+      console.log(aimObj);
+    }
   }
 });
-
-//parse the array
-for (var i = 0; i < csvData.length; i++){
-    var jsonObj = csvData[i];
-    let aimObj = backend.jsonToAim(jsonObj);
-}
 
 /*function saveJson(text, filename){
     var a = document.createElement('a');

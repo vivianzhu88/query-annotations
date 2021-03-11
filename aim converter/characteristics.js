59816901920ea3001c306ffa053bdb98ccaf8d63
vivianzhu88 <31047942+vivianzhu88@users.noreply.github.com>
2021-03-10 17:57:28 -0800

    add characteristics file

---
+++ aim converter/characteristics.js
@@ -0,0 +1,372 @@
+var c = {};
+
+c["subtlety1"] = {
+    "code": "extremely subtle",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "extremely subtle",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "subtlety"
+}
+c["subtlety2"] = {
+    "code": "very subtle",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "very subtle",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "subtlety"
+}
+c["subtlety3"] = {
+    "code": "fairly subtle",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "fairly subtle",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "subtlety"
+}
+c["subtlety4"] = {
+    "code": "somewhat subtle",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "somewhat subtle",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "subtlety"
+}
+c["subtlety5"] = {
+    "code": "obvious",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "obvious",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "subtlety"
+}
+
+c["internal structure1"] = {
+    "code": "C12471",
+    "codeSystemName": "NCIT",
+    "codeSystemVersion": "1.0",
+    "displayValue": "soft tissue",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "internal structure"
+}
+c["internal structure2"] = {
+    "code": "RID1547",
+    "codeSystemName": "RadLex",
+    "codeSystemVersion": "1.0",
+    "displayValue": "fluid",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "internal structure"
+}
+c["internal structure3"] = {
+    "code": "RID1538",
+    "codeSystemName": "RadLex",
+    "codeSystemVersion": "1.0",
+    "displayValue": "fat",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "internal structure"
+}
+c["internal structure4"] = {
+    "code": "15158005",
+    "codeSystemName": "SRT",
+    "codeSystemVersion": "1.0",
+    "displayValue": "air",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "internal structure"
+}
+
+c["calcification1"] = {
+    "code": "popcorn",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "popcorn",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "calcification"
+}
+c["calcification2"] = {
+    "code": "laminated",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "laminated",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "calcification"
+}
+c["calcification3"] = {
+    "code": "RID5741",
+    "codeSystemName": "RadLex",
+    "codeSystemVersion": "1.0",
+    "displayValue": "solid",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "calcification"
+}
+c["calcification4"] = {
+    "code": "non-central",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "non-central",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "calcification"
+}
+c["calcification5"] = {
+    "code": "RID5827",
+    "codeSystemName": "Radlex",
+    "codeSystemVersion": "1.0",
+    "displayValue": "central",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "calcification"
+}
+c["calcification6"] = {
+    "code": "RID28473",
+    "codeSystemName": "Radlex",
+    "codeSystemVersion": "1.0",
+    "displayValue": "absent",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "calcification"
+}
+
+c["sphericity1"] = {
+    "code": "RID5811",
+    "codeSystemName": "RadLex",
+    "codeSystemVersion": "1.0",
+    "displayValue": "linear",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "sphericity"
+}
+c["sphericity2"] = {
+    "code": "linear-oval",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "linear-oval",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "sphericity"
+}
+c["sphericity3"] = {
+    "code": "RID5800",
+    "codeSystemName": "RadLex",
+    "codeSystemVersion": "1.0",
+    "displayValue": "oval",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "sphericity"
+}
+c["sphericity4"] = {
+    "code": "oval-round",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "oval-round",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "sphericity"
+}
+c["sphericity5"] = {
+    "code": "RID5799",
+    "codeSystemName": "RadLex",
+    "codeSystemVersion": "1.0",
+    "displayValue": "round",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "sphericity"
+}
+
+c["margin1"] = {
+    "code": "poorly definied",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "poorly definied",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "margin"
+}
+c["margin2"] = {
+    "code": "somewhat defined",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "somewhat defined",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "margin"
+}
+c["margin3"] = {
+    "code": "fairly defined",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "fairly defined",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "margin"
+}
+c["margin4"] = {
+    "code": "very defined",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "very defined",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "margin"
+}
+c["margin5"] = {
+    "code": "sharp margin",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "sharp margin",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "margin"
+}
+
+c["lobulation1"] = {
+    "code": "no lobulation",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "no lobulation",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "lobulation"
+}
+c["lobulation2"] = {
+    "code": "somewhat lobulation",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "somewhat lobulation",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "lobulation"
+}
+c["lobulation3"] = {
+    "code": "fairly lobulated",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "fairly lobulated",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "lobulation"
+}
+c["lobulation4"] = {
+    "code": "very lobulated",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "very lobulated",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "lobulation"
+}
+c["lobulation5"] = {
+    "code": "marked lobulation",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "marked lobulation",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "lobulation"
+}
+
+c["spiculation1"] = {
+    "code": "no spiculation",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "no spiculation",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "spiculation"
+}
+c["spiculation2"] = {
+    "code": "somewhat spiculation",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "somewhat spiculation",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "spiculation"
+}
+c["spiculation3"] = {
+    "code": "fairly spiculated",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "fairly spiculated",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "spiculation"
+}
+c["spiculation4"] = {
+    "code": "very spiculated",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "very spiculated",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "spiculation"
+}
+c["spiculation5"] = {
+    "code": "marked spiculation",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "marked spiculation",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "spiculation"
+}
+
+c["texture1"] = {
+    "code": "non-solid/ground glass opacity",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "non-solid/ground glass opacity",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "texture"
+}
+c["texture2"] = {
+    "code": "somewhat solid",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "somewhat solid",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "texture"
+}
+c["texture3"] = {
+    "code": "part solid/mixed",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "part solid/mixed",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "texture"
+}
+c["texture4"] = {
+    "code": "mostly solid",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "mostly solid",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "texture"
+}
+c["texture5"] = {
+    "code": "solid texture",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "solid texture",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "texture"
+}
+
+c["malignancy1"] = {
+    "code": "highly unlikely for cancer",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "highly unlikely for cancer",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "malignancy"
+}
+c["malignancy2"] = {
+    "code": "moderately unlikely for cancer",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "moderately unlikely for cancer",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "malignancy"
+}
+c["malignancy3"] = {
+    "code": "indeterminate likelihood for cancer",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "indeterminate likelihood for cancer",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "malignancy"
+}
+c["malignancy4"] = {
+    "code": "moderately suspicious for cancer",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "moderately suspicious for cancer",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "malignancy"
+}
+c["malignancy5"] = {
+    "code": "highly suspicious for cancer",
+    "codeSystemName": "LIDC-IDRI",
+    "codeSystemVersion": "",
+    "displayValue": "highly suspicious for cancer",
+    "displayXmlns": "uri:iso.org:21090",
+    "labelValue": "malignancy"
+}
+
+exports.charDict = c;
